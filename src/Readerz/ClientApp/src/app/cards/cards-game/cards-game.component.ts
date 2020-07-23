6f2b141ec76946ff20df64d24660384b638a608d
Zeinekken-hub <58776590+Zeinekken-hub@users.noreply.github.com>
2020-07-23 23:01:39 +0300

    add card-game.service.ts

--- src/Readerz/ClientApp/src/app/cards/cards-game/cards-game.component.ts
+++ src/Readerz/ClientApp/src/app/cards/cards-game/cards-game.component.ts
@@ -4,6 +4,7 @@ import { CardSetService } from 'src/app/services/card-set.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { fromEvent, from, of, Subscription } from 'rxjs';
 import { map, filter, delay, mergeMap } from 'rxjs/operators';
+import { CardGameService, GameKey } from 'src/app/services/card-game.service';
 
 @Component({
   selector: 'app-cards-game',
@@ -13,12 +14,12 @@ import { map, filter, delay, mergeMap } from 'rxjs/operators';
 export class CardsGameComponent implements OnInit {
   cardSetId: number
   text: string
-  game: CardGameProvider
   private eventSubscription: Subscription
 
   constructor(
     private cardService: CardService,
-    activedRoute: ActivatedRoute
+    activedRoute: ActivatedRoute,
+    private game: CardGameService
   ) {
     this.cardSetId = Number.parseInt(activedRoute.snapshot.params["id"]);
   }
@@ -26,7 +27,10 @@ export class CardsGameComponent implements OnInit {
   ngOnInit(): void {
     this.cardService.getByCardSet(this.cardSetId)
       .subscribe(
-        val => this.game = new CardGameProvider(val.cards),
+        val => { 
+          this.game.cards = val.cards;
+          this.game.randomize()
+        },
         err => console.log(err),
         () => this.setHandler()
       );
@@ -56,58 +60,5 @@ export class CardsGameComponent implements OnInit {
   }
 
   //todo take and move a card with word to left or right
-}
-
-class CardGameProvider {
-  cards: Card[];
-  currentCard: Card;
-  currentLength: number;
-  currentCardIndex: number;
-  currentCardSide: CardSide;
-  end: boolean = false;
-
-  constructor(cards: Card[]) {
-    this.cards = cards;
-    this.currentCard = cards[0];
-    this.currentLength = this.cards.length;
-    this.currentCardIndex = 0;
-    this.currentCardSide = CardSide.Front;
-  }
-
-  pushCurrentToEnd() {
-    this.cards.push(this.cards[this.currentCardIndex]);
-  }
-
-  nextCard() {
-    if (this.currentCardIndex + 1 == this.currentLength) {
-      this.cards.splice(0, this.currentLength);
-      this.currentLength = this.cards.length;
-      this.currentCardIndex = 0;
-    } else {
-      this.currentCardIndex++;
-    }
-
-    if (this.currentLength === 0) {
-      this.end = true;
-    }
-
-    this.currentCard = this.cards[this.currentCardIndex];
-  }
-
-  get text() {
-    return this.currentCard[this.currentCardSide];
-  }
-
-  swapTextSides() {
-    this.currentCardSide = this.currentCardSide === CardSide.Front ? CardSide.Back : CardSide.Front;
-  }
-}
-
-enum CardSide {
-  Front = "front", Back = "back"
-}
-enum GameKey {
-  Enter = "Enter",
-  Spacebar = " "
 }
 
---
+++ src/Readerz/ClientApp/src/app/services/card-game.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from "@angular/core";
+import { Card } from "./card.service";
+
+@Injectable()
+export class CardGameService {
+    private _cards: Card[];
+    private _currentCard: Card;
+    private _currentCardSide: CardSide;
+    end: boolean = false;
+    currentLength: number;
+    currentCardIndex: number;
+
+    set cards(value: Card[]) {
+        this._cards = value;
+        this._currentCard = value[0];
+        this._currentCardSide = CardSide.Front;
+        this.currentLength = value.length;
+        this.currentCardIndex = 0;
+    }
+
+    get text() {
+        return this._currentCard[this._currentCardSide];
+    }
+
+    pushCurrentToEnd() {
+        this._cards.push(this._cards[this.currentCardIndex]);
+    }
+
+    nextCard() {
+        if (this.currentCardIndex + 1 == this.currentLength) {
+            this._cards.splice(0, this.currentLength);
+            this.currentLength = this._cards.length;
+            this.currentCardIndex = 0;
+        } else {
+            this.currentCardIndex++;
+        }
+
+        if (this.currentLength === 0) {
+            this.end = true;
+        }
+
+        this._currentCard = this._cards[this.currentCardIndex];
+    }
+
+    swapTextSides() {
+        this._currentCardSide = this._currentCardSide === CardSide.Front ? CardSide.Back : CardSide.Front;
+    }
+
+    randomize() {
+        let c: number = this._cards.length;
+
+        while(c > 0) {
+            let index = Math.floor(Math.random() * c);
+            c--;
+            [this._cards[c], this._cards[index]] = [this._cards[index], this._cards[c]];
+        }
+    }
+}
+
+export enum CardSide {
+    Front = "front", Back = "back"
+}
+export enum GameKey {
+    Enter = "Enter",
+    Spacebar = " "
+}
