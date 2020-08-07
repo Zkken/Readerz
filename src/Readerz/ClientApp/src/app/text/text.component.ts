c1d863d1c3be9ad85ded2c6ca1e7604ea01563f4
Zeinekken <zeinekken@pop-os.localdomain>
2020-08-07 20:40:11 +0300

    add text errors and word translation

--- src/Readerz/ClientApp/src/app/text/text.component.ts
+++ src/Readerz/ClientApp/src/app/text/text.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild, TemplateRef, ViewContainerRef, AfterViewInit, ChangeDetectorRef,  Input } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ViewContainerRef, AfterViewInit, ChangeDetectorRef, Input } from '@angular/core';
 import { UnknownText } from '../models/text';
 import { Util } from '../services/util.service';
 import { filter, switchMap } from 'rxjs/operators';
 import { from, of } from 'rxjs';
 import { TextService, Language } from '../services/text.service';
+import { Card } from '../services/card.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-text',
@@ -19,17 +21,34 @@ export class TextComponent implements OnInit, AfterViewInit {
   languages: Language[]
   languageFrom: string
   languageTo: string
+  lastTranslation: string = "Word that will be traslated will be here"
+  translation: string = "Translation will be here"
+  cards: Card[]
+  selectForm = this.fb.group({
+    langToSelect: ['', [Validators.required]],
+    langFromSelect: ['', [Validators.required]]
+  })
 
-  constructor(private cdr: ChangeDetectorRef, private textService: TextService) {
+  constructor(private cdr: ChangeDetectorRef, private textService: TextService,
+    public fb: FormBuilder) {
     this.textService.getSupportedLanguages().subscribe(val => {
       this.languages = val.languages;
     })
+  }
+
+  get langToSelect() {
+    return this.selectForm.get('langToSelect');
+  }
+
+  get langFromSelect() {
+    return this.selectForm.get('langFromSelect');
   }
 
   ngOnInit() {
   }
 
   //dom generating with view
+  //TODO: need to comment this section
   ngAfterViewInit() {
     from((this.text.innerText.split('\n'))).pipe(
       filter(val => !Util.isBlank(val)),
@@ -46,20 +65,35 @@ export class TextComponent implements OnInit, AfterViewInit {
   //TODO bug: sentence: I love sushi., there are 3 words ["I", "love", "sushi."], 
   //but sushi has the end with dot, need to fix this case and similar with other punctuations.
 
-
-
   selectWord(event: any) {
+    //check for validate errors
+    if (this.langToSelect.errors || this.langFromSelect.errors) {
+      return;
+    }
+    //traslate the span inner text
     let span = event.target as HTMLSpanElement
-
     this.textService.translateWord({
       text: span.innerText,
       from: this.languageFrom,
       to: this.languageTo
     }).subscribe(val => {
-      console.log(val.translations);
+      this.lastTranslation = span.innerText;
+      this.translation = val.translations.join(', ');
     });
 
     span.className = "border rounded bg-info";
   }
 
+  addWordToCards() {
+    //check for validate errors
+    if (this.langToSelect.errors || this.langFromSelect.errors) {
+      return;
+    }
+    //check for existings last translation 
+    if(!this.lastTranslation || !this.translation) {
+      return;
+    }
+    //push new card to card array
+    this.cards.push(new Card(null, this.lastTranslation, this.translation));
+  }
 }
