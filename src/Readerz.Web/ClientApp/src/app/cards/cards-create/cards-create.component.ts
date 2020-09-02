f361aefc12258c3cf1aaee96534704e5b0096e2b
Zeinekken-hub <bogdanmuha5@gmail.com>
2020-09-02 08:05:45 +0300

    /cards/create validation

--- src/Readerz.Web/ClientApp/src/app/cards/cards-create/cards-create.component.ts
+++ src/Readerz.Web/ClientApp/src/app/cards/cards-create/cards-create.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardService } from 'src/app/services/card.service';
 import { Router } from '@angular/router';
 import { CardSet } from 'src/app/models/card-set';
 import { CardSetService } from 'src/app/services/card-set.service';
 import { Card } from 'src/app/models/card';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, Validators } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-cards-create',
@@ -12,24 +16,61 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./cards-create.component.css']
 })
 export class CardsCreateComponent implements OnInit {
-  cards: Card[]
-  card: Card
-  cardSet: CardSet
+  cards: Card[];
+  card: Card;
+  cardSet: CardSet;
+
+  cardSetForm = this.formBuilder.group({
+    select: ['', [Validators.required]],
+    cardSetName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+    front: ['', [Validators.required, Validators.maxLength(50)]],
+    back: ['', [Validators.required, Validators.maxLength(50)]]
+  })
 
   constructor(
     private cardSetService: CardSetService,
     private router: Router,
-    private cardService: CardService
+    private cardService: CardService,
+    public formBuilder: FormBuilder
   ) {
     this.cards = [];
     this.cardSet = new CardSet();
     this.card = new Card();
   }
 
+  get select() {
+    return this.cardSetForm.get('select');
+  }
+
+  get cardSetName() {
+    return this.cardSetForm.get('cardSetName');
+  }
+
+  get back() {
+    return this.cardSetForm.get('back');
+  }
+
+  get front() {
+    return this.cardSetForm.get('front');
+  }
+
   ngOnInit() {
+    fromEvent(document, 'keypress').pipe(
+      filter((keyEvent: KeyboardEvent) => keyEvent.key === Key.ENTER)
+    )
+    .subscribe(() => {
+      this.add();
+    })
   }
 
   add() {
+    if(this.front.invalid || this.back.invalid) {
+      return;
+    }
+    this.front.setValue('');
+    this.front.markAsUntouched();
+    this.back.setValue('');
+    this.back.markAsUntouched();
     this.cards.push(this.card);
     this.card = new Card();
   }
