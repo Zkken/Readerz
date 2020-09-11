b7a71ad3e84dc69ba0bf83333b990230fcbe01ba
Zeinekken-hub <bogdanmuha5@gmail.com>
2020-09-11 06:24:43 +0300

    add ng-localize

---
+++ src/Readerz.Application/Common/Helpers/QueryableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.DynamicLinq;
+using Readerz.Application.Common.Exceptions;
+
+namespace Readerz.Application.Common.Helpers
+{
+    public static class Check
+    {
+        /// <summary>
+        /// Check if entity exists by primary key value. If not throws a not found exception.
+        /// </summary>
+        /// <param name="source">IQueryable source.</param>
+        /// <param name="primaryKey">Entity's primary key which represented as an expression.</param>
+        /// <param name="id">Primary key's value.</param>
+        /// <typeparam name="TEntity">Entity type.</typeparam>
+        /// <typeparam name="TKey">Primary key type.</typeparam>
+        public static async void TryEntityExistsAsync<TEntity, TKey>(this IQueryable<TEntity> source,
+            Expression<Func<TEntity, TKey>> primaryKey, TKey id)
+        {
+            //e.x. input: prop.Id output: Id
+            var pk = primaryKey.Body.ToString().Split('.').Last();
+            var query = $"e => e.{pk} == {id}";
+            var entity = await source.FirstOrDefaultAsync(query);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TEntity), id);
+            }
+        }
+    }
+}
